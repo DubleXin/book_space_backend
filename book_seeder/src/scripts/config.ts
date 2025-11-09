247b81c55a562ad015c30bfaaaaecbe2d63a75dc
DubleXin <progames720.2@gmail.com>
2025-11-09 17:26:57 +0100

    feature:book_seeder

---
+++ book_seeder/src/scripts/config.ts
@@ -0,0 +1,16 @@
+export const OPEN_LIBRARY_BASE = "https://openlibrary.org";
+export const SUBJECTS = [
+  "fantasy",
+  "science_fiction",
+  "romance",
+  "history",
+  "programming",
+  "horror",
+  "humor",
+  "kids_books",
+  "religion",
+  "mathematics",
+];
+export const BOOKS_PER_SUBJECT = 20;
+export const USER_AGENT = `DiplomaBookFetcher/1.0 (${process.env.USER_AGENT_CREDITS})`;
+export const BOOKS_JSON_PATH = "../seed/books.json";
